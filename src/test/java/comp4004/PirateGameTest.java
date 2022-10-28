90b7ee9e678e9093c9d271d02b8bfd526822bf92
BRM6 <madimadi88@icloud.com>
2022-10-28 19:39:39 -0400

    A-test commit
    row45

--- src/test/java/comp4004/PirateGameTest.java
+++ src/test/java/comp4004/PirateGameTest.java
@@ -2,11 +2,15 @@ package comp4004;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PirateGameTest {
     private ByteArrayOutputStream testOut;
@@ -33,5 +37,31 @@ class PirateGameTest {
         System.setIn(systemIn);
         System.setOut(systemOut);
     }
+
+    //PART 1
+    @Test
+    @DisplayName("test row 45")
+    void testRow45() {
+        //init
+        Player p = new Player("Di");
+        game.drawForturnCard(p);
+        p.setFortuneCard("coin");
+        game.setNewPlayer(p);
+        String[] die = new String[8];
+
+        for (int i=0; i<8; i++){               //roll die
+            die[i] = game.rollSingleDie();
+        }
+        for (int i=0; i<8; i++){            //assign dies
+            if (i<3){
+                die[i] = "skull";
+            }else {
+                die[i] = "sword";
+            }
+        }
+        int final_score = game.scoreForKindsAndChest(die, p) + game.scoreForDC(die, p);
+        assertEquals(0, final_score);
+    }
+
 
 }
