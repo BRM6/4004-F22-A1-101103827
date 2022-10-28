cb7d11c22e2ceedcb51d228a597afc88043b1f63
BRM6 <madimadi88@icloud.com>
2022-10-28 18:40:30 -0400

    set up commit
    this commit is for A-test file and U-test file

--- src/test/java/comp4004/PirateGameTest.java
+++ src/test/java/comp4004/PirateGameTest.java
@@ -1,4 +1,37 @@
 package comp4004;
 
-public class PirateGameTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+class PirateGameTest {
+    private ByteArrayOutputStream testOut;
+    private final PrintStream systemOut = System.out;
+    private final InputStream systemIn = System.in;
+    private ByteArrayInputStream testIn;
+
+    PirateGame game;
+
+    private void scannerInput(String data) {
+        testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    @BeforeEach
+    void creatingGame(){
+        game = new PirateGame();
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+
+    @AfterEach
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
+
 }
--- src/test/java/comp4004/PirateGameUnitTest.java
+++ src/test/java/comp4004/PirateGameUnitTest.java
@@ -1,5 +1,36 @@
 package comp4004;
 
-public class PirateGameUnitTest {
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+class PirateGameUnitTest {
+    private ByteArrayOutputStream testOut;
+    private final PrintStream systemOut = System.out;
+    private final InputStream systemIn = System.in;
+    private ByteArrayInputStream testIn;
+
+    PirateGame game;
+
+    private void scannerInput(String data) {
+        testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    @BeforeEach
+    void creatingGame(){
+        game = new PirateGame();
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+
+    @AfterEach
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
 }
