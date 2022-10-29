6f234a2ec9dd60e8138f941eb9ce9bc27313a190
BRM6 <madimadi88@icloud.com>
2022-10-28 20:09:02 -0400

    Socket commit
    code for game logic in server side

--- src/main/java/comp4004/GameServer.java
+++ src/main/java/comp4004/GameServer.java
@@ -41,7 +41,7 @@ public class GameServer extends Thread implements Serializable {
     public void run(){
         try{
             acceptConnections();
-//            gameLoop();
+            gameLoop();
             ss.close();
         } catch (ClassNotFoundException e){
             e.printStackTrace();
@@ -57,9 +57,152 @@ public class GameServer extends Thread implements Serializable {
     }
 
 
+    public void gameLoop(){
+        try{
+            playerServer[0].sendPlayer(players);
+            playerServer[1].sendPlayer(players);
+            playerServer[2].sendPlayer(players);
+
+            //recording of players deduction point
+            int player0Deduct = 0;
+            int player1Deduct = 0;
+            int player2Deduct = 0;
 
 
 
+            while(true){
+
+                rounds++;
+
+                //players playing
+                System.out.println("ROUND " + rounds);
+
+                //player 0 is playing
+                playerServer[0].sendRoundNo(rounds);
+                playerServer[0].sendScore(players);
+                int player0Receive = playerServer[0].receiveScore();
+                System.out.println("Player 0 received score is : " + player0Receive);
+                System.out.println();
+                //if player 0 received num of skull, which is below 0, player 0 will deduct points from player 1 2
+                if (player0Receive < 0){
+                    player1Deduct = deductPoint(player0Receive);
+                    player2Deduct = deductPoint(player0Receive);
+                    System.out.println("PLAYER 0 AT SKULL ISLAND DEDUCT " + deductPoint(player0Receive) + "FROM PLAYER 1 AND PLAYER 2");
+
+                    System.out.println("NOW CHECKING IF PLAYER 1 AND PLAYER 2 POINTS BELOW 0, IF ITS, SET TO 0");
+                    // checking if player 1 and player 2 score is below 0, if it is, set to 0
+                    if (players[1].getScore() < Math.abs(deductPoint(player0Receive))){
+                        player1Deduct = (players[1].getScore() * -1);
+                    }
+                    if (players[2].getScore() < Math.abs(deductPoint(player0Receive))){
+                        player2Deduct = (players[2].getScore() * -1);
+                    }
+                    players[0].setScore(players[0].getScore());
+                    players[1].setScore(players[1].getScore() + player1Deduct);
+                    players[2].setScore(players[2].getScore() + player2Deduct);
+                    player1Deduct = 0; // reset player 1 deduct to 0
+                    player2Deduct = 0; // reset player 2 deduct to 0
+                }
+                else{
+                    players[0].setScore(player0Receive + player0Deduct);
+                    player0Deduct = 0; // rest player 0's deduct to 0
+                }
+
+                if (players[0].getScore() >= 3000){
+                    break;
+                }
+
+                //player 1 is playing
+                playerServer[1].sendRoundNo(rounds);
+                playerServer[1].sendScore(players);
+                int player1Receive = playerServer[1].receiveScore();
+                System.out.println("Player 1 received score is : " + player1Receive);
+                System.out.println();
+                //if player 1 received num of skull, which is below 0, player 1 will deduct points from player 0 2
+                if (player1Receive < 0){
+                    player0Deduct = deductPoint(player1Receive);
+                    player2Deduct = deductPoint(player1Receive);
+                    System.out.println("PLAYER 1 AT SKULL ISLAND DEDUCT " + deductPoint(player1Receive) + " FROM PLAYER 0 AND PLAYER 2");
+                    System.out.println("NOW CHECKING IF PLAYER 0 AND PLAYER 2 POINTS BELOW 0, IF ITS, SET TO 0");
+                    // checking if player 0 and player 2 score is below 0, if it is, set to 0
+                    if (players[0].getScore() < Math.abs(deductPoint(player1Receive))){
+                        player0Deduct = (players[0].getScore() * -1);
+                    }
+                    if (players[2].getScore() < Math.abs(deductPoint(player1Receive))){
+                        player2Deduct = (players[2].getScore() * -1);
+                    }
+                    players[0].setScore(players[0].getScore() + player0Deduct);
+                    players[1].setScore(players[1].getScore());
+                    players[2].setScore(players[2].getScore() + player2Deduct);
+                    player0Deduct = 0; // reset player 0 deduct to 0
+                    player2Deduct = 0; // reset player 2 deduct to 0
+                }
+                else{
+                    players[1].setScore(player1Receive + player1Deduct);
+                    player1Deduct = 0; // reset player 1's deduct to 0
+                }
+
+                if (players[1].getScore() >= 3000){
+                    break;
+                }
+
+                //player 2 is playing
+                playerServer[2].sendRoundNo(rounds);
+                playerServer[2].sendScore(players);
+                int player2Receive = playerServer[2].receiveScore();
+                System.out.println("Player 2 received score is : " + player2Receive);
+                System.out.println();
+                //if player 2 received num of skull, which is below 0, player 2 will deduct points from player 0 1
+                if (player2Receive < 0){
+                    player0Deduct = deductPoint(player2Receive);
+                    player1Deduct = deductPoint(player2Receive);
+                    System.out.println("PLAYER 2 AT SKULL ISLAND DEDUCT " + deductPoint(player2Receive) + "FROM PLAYER 0 AND PLAYER 1");
+
+                    System.out.println("NOW CHECKING IF PLAYER 0 AND PLAYER 1 POINTS BELOW 0, IF ITS, SET TO 0");
+                    // checking if player 0 and player 1 score is below 0, if it is, set to 0
+                    if (players[0].getScore() < Math.abs(deductPoint(player2Receive))){
+                        player0Deduct = (players[0].getScore() * -1);
+                    }
+                    if (players[1].getScore() < Math.abs(deductPoint(player2Receive))){
+                        player1Deduct = (players[1].getScore() * -1);
+                    }
+                    players[0].setScore(players[0].getScore() + player0Deduct);
+                    players[1].setScore(players[1].getScore() + player1Deduct);
+                    players[2].setScore(players[2].getScore());
+                    player0Deduct = 0; // reset player 0 deduct to 0
+                    player1Deduct = 0; // reset player 1 deduct to 0
+                }
+                else{
+                    players[2].setScore(player2Receive + player2Deduct);
+                    player2Deduct = 0; // rest player 1's deduct to 0
+                }
+
+                if (players[2].getScore() >= 3000){
+                    break;
+                }
+            }
+
+            //winner already generated
+            playerServer[0].sendRoundNo(-1);
+            playerServer[1].sendRoundNo(-1);
+            playerServer[2].sendRoundNo(-1);
+
+            playerServer[0].sendScore(players);
+            playerServer[1].sendScore(players);
+            playerServer[2].sendScore(players);
+
+            //*******************generate winner need to fill up
+
+            Player p = game.getWinner(players);
+            System.out.println("The winner is " + p.getName());
+            for (int i = 0; i < playerServer.length; i++) {
+                playerServer[i].dOut.writeObject(p);
+                playerServer[i].dOut.flush();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     synchronized public void acceptConnections() throws ClassNotFoundException {
         try{
