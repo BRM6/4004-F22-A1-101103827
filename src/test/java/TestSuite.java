2e8f1fd674b87cf14c9fad3dc5d3d000e233c6bd
BRM6 <madimadi88@icloud.com>
2022-10-28 20:10:09 -0400

    TestSuite commit
    created test suite file

---
+++ src/test/java/TestSuite.java
@@ -0,0 +1,9 @@
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SelectPackages("comp4004")
+@SuiteDisplayName("a tests and u test ")
+public class TestSuite {
+}
