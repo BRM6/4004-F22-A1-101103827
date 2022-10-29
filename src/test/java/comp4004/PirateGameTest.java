cf8886d12496cf0de7f4e9df0161d7515320ecdb
BRM6 <madimadi88@icloud.com>
2022-10-28 20:01:23 -0400

    A-test commit
    row 123

--- src/test/java/comp4004/PirateGameTest.java
+++ src/test/java/comp4004/PirateGameTest.java
@@ -1674,6 +1674,35 @@ class PirateGameTest {
         assertEquals(0, final_score);
     }
 
+    @Test
+    @DisplayName("test row 123")
+    void testRow123() {
+        //init
+        Player p = new Player("Di");
+        game.drawForturnCard(p);
+        p.setFortuneCard("4 sword");
+        game.setNewPlayer(p);
+        String[] die = new String[8];
+
+        for (int i=0; i<8; i++){               //roll die
+            die[i] = game.rollSingleDie();
+        }
+        for (int i=0; i<8; i++) {            //assign dies
+            if (i < 3) {
+                die[i] = "monkey";
+            }
+            if (i >= 3 && i < 7) {
+                die[i] = "sword";
+            }
+            if (i >= 7 && i < 8) {
+                die[i] = "skull";
+            }
+        }
+        game.seaBattle(p, die);
+        int final_score = p.getScore();
+        assertEquals(1300, final_score);
+    }
+
 
 
 
